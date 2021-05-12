b17e03faad70378ef089fcec1c18019b7229a56f
System Administrator <boris_moers@rcomext.com>
2021-05-12 18:15:58 +0200

    Changes by 'System Administrator' on 'regular change 2 (missing?)' -- Build #1

---
+++ sn/01 CICD Test App/Script Include/active_true/x_11413_cicd_t_01.hello_world_3333.js
@@ -0,0 +1,17 @@
+/* 
+ * Application : 01 CICD Test App
+ * ClassName   : sys_script_include
+ * Created On  : 2021-05-12 15:17:35
+ * Created By  : admin
+ * Updated On  : 2021-05-12 15:17:35
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=cff19ac22f88f810d8455aab2799b6e8
+ */
+var hello_world_3333 = Class.create();
+hello_world_3333.prototype = {
+    initialize: function() {
+		return false;
+    },
+
+    type: 'hello_world_3333'
+};
