2937ea496a73592f949be3190f78aa31d978a8e6
bmoers <boris@moers.ch>
2021-05-12 18:10:49 +0200

    'master' branch updated from  https://dev54289.service-now.com no-cicd

---
+++ sn/01 CICD Test App/Script Include/active_true/x_11413_cicd_t_01.hello_world.js
@@ -0,0 +1,18 @@
+/* 
+ * Application : 01 CICD Test App
+ * ClassName   : sys_script_include
+ * Created On  : 2020-08-31 12:33:26
+ * Created By  : admin
+ * Updated On  : 2020-09-07 10:23:53
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=ce98ad14db035410fcf41780399619e0
+ */
+var hello_world = Class.create();
+hello_world.prototype = {
+    initialize: function() {
+		// change on target 222 222
+		return false;
+    },
+
+    type: 'hello_world'
+};
